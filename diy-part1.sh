791e42fdd1176281ce935d3abc390dbbf2cd2362
Joee <32849562+Joee-D@users.noreply.github.com>
2023-04-24 16:50:41 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,7 +15,6 @@
 
 git clone -b lede https://github.com/pymumu/luci-app-smartdns.git tmp/smartdns_luci/luci-app-smartdns
 git clone https://github.com/pymumu/openwrt-smartdns.git tmp/smartdns/smartdns
-# git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git tmp/argon_luci/luci-theme-argon
 
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
@@ -24,4 +23,3 @@ echo 'src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;lu
 echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >>feeds.conf.default
 echo 'src-cpy smartdns tmp/smartdns' >>feeds.conf.default
 echo 'src-cpy smartdns_luci tmp/smartdns_luci' >>feeds.conf.default
-# echo 'src-cpy argon_luci tmp/argon_luci' >>feeds.conf.default
