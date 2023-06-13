e92215ca104f12ba01f31d1df9b6b4507d3d2289
Joee <32849562+Joee-D@users.noreply.github.com>
2023-06-13 11:58:45 +0800

    Update openwrt-diy-part2.sh

--- openwrt-diy-part2.sh
+++ openwrt-diy-part2.sh
@@ -72,7 +72,7 @@ cd ../../..
 sed -i 's/UTC/CST-8/g' package/base-files/files/bin/config_generate
 
 # 添加 'i915/glk_dmc_ver1_04.bin' 到5.15的内核
-sed -i '$r firmware-i915' package/firmware/linux-firmware/intel.mk
+sed -i '$r i915-firmware' package/firmware/linux-firmware/intel.mk
 echo 'CONFIG_FIRMWARE_IN_KERNEL=y' >> target/linux/x86/64/config-5.15
 echo 'CONFIG_EXTRA_FIRMWARE="i915/glk_dmc_ver1_04.bin"' >> target/linux/x86/64/config-5.15
 echo 'CONFIG_EXTRA_FIRMWARE_DIR="/lib/firmware"' >> target/linux/x86/64/config-5.15
