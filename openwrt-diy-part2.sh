e5fe3c163c49cd93b6963b8b577b77f830dff5c9
Joee <32849562+Joee-D@users.noreply.github.com>
2024-04-10 22:07:52 +0800

    support passwall

--- openwrt-diy-part2.sh
+++ openwrt-diy-part2.sh
@@ -34,7 +34,7 @@ mkdir package/others
 
 # passwall
 git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall-packages.git package/others/passwall_pkg
-git clone --depth 1 -b luci https://github.com/xiaorouji/openwrt-passwall.git package/others/passwall
+git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall.git package/others/passwall
 #git clone --depth 1 -b luci-smartdns-new-version https://github.com/xiaorouji/openwrt-passwall.git package/others/passwall
 #git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall2.git package/others/passwall2
 
