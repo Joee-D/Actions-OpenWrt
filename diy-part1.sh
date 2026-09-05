5e6e141aee64744419c5d45759d91fb610127343
Joee <qiaoqiao12358@gmail.com>
2026-09-05 23:17:26 +0800

    Simplify OpenWrt build workflow and cache toolchain

--- diy-part1.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Actions-OpenWrt: runs before feeds are updated
-echo "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main" >> "feeds.conf.default"
--- diy-part2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Actions-OpenWrt: runs after feeds are installed
-
-# Keep Moby from copying host executables on GitHub runners
-if [ -x /usr/local/bin/runc ]; then
-	sudo mv /usr/local/bin/runc /usr/local/bin/runc.disabled
-fi
